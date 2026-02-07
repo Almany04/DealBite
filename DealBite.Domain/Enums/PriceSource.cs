2ccd002b281386949643af06b320d95c6b70ae41
Almany04 <bencealmas@gmail.com>
2026-02-07 22:45:31 +0100

    Add project files.

---
+++ DealBite.Domain/Enums/PriceSource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DealBite.Domain.Enums
+{
+    public enum PriceSource
+    {
+        GvhArfigyelo, PdfScraping, WebScraping, Manual
+    }
+}
---
+++ DealBite.Domain/Enums/ProductUnit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DealBite.Domain.Enums
+{
+    public enum ProductUnit
+    {
+        Kg, Db, L, Dl, Ml, Csomag, Dkg, g
+    }
+}
